98cd3db9f42f838d4c54b44149c862dc880c775b
Florine <florinevinet@gmail.com>
2024-04-02 16:41:32 +0200

    data done

---
+++ src/data.js
@@ -0,0 +1,81 @@
+export const marioCharacters = [
+    {
+        characterName: "Mario",
+        characterDescription: "The main hero of the Mushroom Kingdom. Mario is always bright and cheerful and instantly recognizable with his blue overalls, red cap, and trademark moustache.",
+        characterImage: "./assets/mario.png",
+        isGood: "true",
+    },
+    {
+        characterName: "Luigi",
+        characterDescription: "Mario's brother and fellow hero of the Mushroom Kingdom",
+        characterImage: "./assets/luigi.png",
+        isGood: "true",
+    },
+    {
+        characterName: "Princess Peach",
+        characterDescription: "The beloved princess of the Mushroom Kingdom. She's extremely kind and is always working to create a world where everyone can live together happily",
+        characterImage: "./assets/peach.png",
+        isGood: "true",
+    },
+    {
+        characterName: "Toad",
+        characterDescription: "A resident of the Mushroom Kingdom who works in service of Princess Peach. Toad has red spots on his head, though others of his kind come in a variety of colors",
+        characterImage: "./assets/toad.png",
+        isGood: "true",
+    },
+    {
+        characterName: "Bowser",
+        characterDescription: "The King of the Koopas. Bowser is Mario's archrival and is always causing trouble in the Mushroom Kingdom.",
+        characterImage: "./assets/bowser.png",
+        isGood: "false",
+    },
+    {
+        characterName: "Yoshi",
+        characterDescription: "Mario's dependable companion who hails from Yoshi's Island ",
+        characterImage: "./assets/yoshi.png",
+        isGood: "true",
+    },
+    {
+        characterName: "Wario",
+        characterDescription: "The self - professed archrival of Mario.Wario wears purple overalls, a yellow hat, and has an instantly recognizable zigzag moustache.",
+        characterImage: "./assets/wario.png",
+        isGood: "false",
+    },
+    {
+        characterName: "Waluigi",
+        characterDescription: "Wario's pal and accomplice. Waluigi is the self-proclaimed rival of Luigi",
+        characterImage: "./assets/waluigi.png",
+        isGood: "false",
+    },
+    {
+        characterName: "Rosalina",
+        characterDescription: "A mysterious lady who travels the galaxy accompanied by her family of star - like creatures called Lumas.",
+        characterImage: "./assets/rosalina.png",
+        isGood: "true",
+    },
+    {
+        characterName: "Bowser Jr.",
+        characterDescription: "The only son of Bowser, the King of the Koopas.He's often seen wearing a mask with an intimidating mouth drawn on it.",
+        characterImage: "./assets/bowserjr.png",
+        isGood: "false",
+    },
+    {
+        characterName: "Boo",
+        characterDescription: "Mischievous ghosts who often appear in dark, abandoned places.",
+        characterImage: "./assets/shyguy.png",
+        isGood: "false",
+    },
+    {
+        characterName: "Donkey kong",
+        characterDescription: "The king of the jungle whose trademark is his red necktie, which bears his initials.",
+        characterImage: "./assets/donkeykong.png",
+        isGood: "true",
+    },
+    {
+        characterName: "Diddy Kong",
+        characterDescription: "Donkey Kong's trusted friend and partner. His trademarks are his red shirt with yellow stars and red cap.",
+        characterImage: "./assets/diddy.png",
+        isGood: "true",
+    },
+
+]
