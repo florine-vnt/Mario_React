961b597fb2c4cb453483e09ac1abe2ce968436f8
MartinFest <martin.berthaud@laposte.net>
2024-04-02 16:04:05 +0200

    création navbar et footerr ✨

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-
+      <Navbar />
+      <h1>Super Mario characters</h1>
+      <Footer />
     </>
   )
 }
